6a8390816012ef925ecc84f5b95c25709e796e68
satyadur <satyakudupudi8500@gmail.com>
2025-07-03 20:56:24 +0530

    assets issue4

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -592,7 +592,7 @@ const Process = () => {
                         data-testid="nasa-link"
                       >
                         <img
-                          src="/assets/nasa.png"
+                          src="/nasa.png"
                           alt="NASA"
                           className="h-5 w-5 object-contain"
                         />
